32f4caf95a995e9cd5db535b966ae5013709ba68
Aircity <aircity@drrr.us>
2019-04-07 18:14:00 +0800

    feat: compile

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TopicModule } from './topic/topic.module';
 import { UserModule } from './user/user.module';
+import { Topic } from './topic/topic.entity';
+import { User } from './user/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TopicModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './database.sqlite3',
+      entities: [Topic, User],
+      synchronize: true,
+    }),
+    TopicModule,
+    UserModule,
+  ],
 })
 export class ApplicationModule {}
