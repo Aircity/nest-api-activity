3b54a9133d74e95e704d0b6949186b447ef6934f
Aircity <aircity@drrr.us>
2019-04-08 10:08:19 +0800

    qa

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { ApplicationModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   app.enableCors({
-    origin: '*'
-  })
-  await app.listen(3000);
+    origin: '*',
+  });
+  await app.listen(3200);
 }
 bootstrap();
